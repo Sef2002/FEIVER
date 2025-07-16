833ca0d57f71e079390dc60add627a7bb78e9c2e
Sef2002 <yousefnakhleh02@gmail.com>
2025-07-16 15:42:45 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import CookieBanner from './components/CookieBanner'; // ✅ IMPORTED
 
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
@@ -35,6 +36,7 @@ function App() {
             <Route path="/prenota/successo" element={<BookingSuccess />} />
           </Routes>
           <Footer />
+          <CookieBanner /> {/* ✅ ADDED */}
         </div>
       </Router>
     </CartProvider>
