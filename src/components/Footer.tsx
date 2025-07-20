c67d5208317c4bd31065bd13ebd2e9738009ff6b
Sef2002 <yousefnakhleh02@gmail.com>
2025-07-20 21:10:36 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Facebook, Instagram, MapPin, Phone, Mail } from 'lucide-react';
+import { Facebook, Instagram, Twitter, MapPin, Phone, Mail } from 'lucide-react';
 
 const Footer: React.FC = () => {
   return (
